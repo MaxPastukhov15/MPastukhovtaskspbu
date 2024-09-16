d1e91ce4c09029a0fef9db3b7614fb286f3c0209
MaxUB <maxub@Ubuntu.myguest.virtualbox.org>
2024-09-16 21:35:29 +0300

    uploading a1

---
+++ a1.cpp
@@ -0,0 +1,11 @@
+/* Maksim Pastukhov st131119@student.spbu.ru or maksimpastukhov15@gmail.com
+ task1*/
+
+#include <iostream>
+#include <string>
+#include "MP.hpp"
+using namespace std;
+void hell() {
+        string h = "Hello, world!";
+        cout << h;
+}
